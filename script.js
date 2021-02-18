8ef07ca011c44984e56fbf5860611010f0cea003
Rachel Strong <rachellynnstrong@gmail.com>
2021-02-17 20:16:29 -0500

    Changed logic to make user build array from which we generate our password

--- script.js
+++ script.js
@@ -1,80 +1,44 @@
-//prompt to generate password - handled via html  
-
-//prompt for length of password (8-128 characters) - handled via range bar element in html 
-
-//prompt for lowercase, uppercase, numeric, and/or special characters 
-
-//user input validation for character types 
-
-//button to generate - handled by assigning function to button in html 
-
-//password is displayed in an alert - handled by displaying in passBox input in html 
-
-
-
-function passwordGen() {
     //variables
     var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lowerCase ="abcdefghijklmnopqrstuvwxyz";
     var Numbers = "1234567890";
     var Special = "!@#$%^&*()_+";
-     //all possible values
-    var allCharacters = upperCase + lowerCase + Numbers + Special;
-    //Uppercase combo values
-    var upperLower = upperCase + lowerCase;
-    var upperNumbers = upperCase + Numbers;
-    var upperSpecial = upperCase + Special;
-    //Lowercase combo values 
-    var lowerNumbers = lowerCase + Numbers;
-    var lowerSpecial = lowerCase + Special;
-    //Numbers combo values
-    var numberSpecial = Numbers + Special;
-    
-    var passLength = document.getElementById('rangeBar').value; //password length identified by the "range bar" value in html 
-    const checkUpper = document.getElementById('Uppercase');
-    console.log(checkUpper.checked);
+ 
+    const caseUpper=document.getElementById('Uppercase'); //grabbing user input  for Uppercase criteria
+    const caseLower=document.getElementById('Lowercase'); //grabbing user input for Lowercase criteria
+    const caseNumbers=document.getElementById('Numbers'); //grabbing user input for Lowercase criteria
+    const caseSpecial=document.getElementById('Special'); //grabbing user input for Lowercase criteria
 
-    const checkLower = document.getElementById('Lowercase');
-    console.log(checkLower.checked);
+    function passwordGen() {   
 
-    const checkNumbers = document.getElementById('Numbers');
-    console.log(checkNumbers.checked);
+        const passLength = document.getElementById('rangeBar').value; //password length identified by the "range bar" value in html
+        var userArray = ""; //array to be modified by user inputs 
+        var password =""; //password array
 
-    const checkSpecial = document.getElementById('Special');
-    console.log(checkSpecial.checked);
+        if (caseUpper.checked) {
+            userArray= userArray+upperCase;
+        }
+        console.log(userArray);
+        if (caseLower.checked) {
+            userArray = userArray+lowerCase;
+        console.log(userArray);
+        }
+        if (caseNumbers.checked){
+            userArray = userArray + Numbers;
+        console.log(userArray);
+        }
+        if (caseSpecial.checked){
+            userArray = userArray+Special;
+            console.log(userArray);
+        }
 
-    var password = "";//starting point 
-    
-    //logic (for loop)
-
- if (checkUpper.checked && checkLower.checked && checkNumbers.checked && checkSpecial.checked) {
         for(var i = 0; i <= passLength; i++) { 
-            password = password + allCharacters.charAt(Math.floor(Math.random() * Math.floor(allCharacters.length - 1)));
-        } 
-    } else if (checkUpper.checked) {
-        for(var i = 0; i <= passLength; i++) { 
-            password = password + upperCase.charAt(Math.floor(Math.random() * Math.floor(upperCase.length - 1)));
+            password = password + userArray.charAt(Math.floor(Math.random() * Math.floor(userArray.length - 1)));
+            console.log(passLength);
         }
-    } else if (checkLower.checked) {
-        for(var i = 0; i <= passLength; i++) {
-            password = password + lowerCase.charAt(Math.floor(Math.random() * Math.floor(lowerCase.length -1)));
-        }
-    } else if (checkNumbers.checked){
-        for(var i = 0; i <= passLength; i++){
-            password = password + Numbers.charAt(Math.floor(Math.random() * Math.floor(Numbers.length - 1))); 
-        }
-    } else if (checkSpecial.checked){
-        for(var i =0; i <= passLength; i++) {
-            password = password + Special.charAt(Math.floor(Math.random() * Math.floor(Special.length - 1)));
-        }
-    } else if (checkUpper.checked && checkLower.checked) {
-        for(var i =0; i <= passLength; i++) {
-            password = password + upperLower.charAt(Math.floor(Math.random() * Math.floor(upperLower.length - 1)));
-        }
+
+        document.getElementById('passBox').value = password;
+        
     }
-    
-    //displaying password
-    
-    document.getElementById('passBox').value = password;
-    
-}
+
+
